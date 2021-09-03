ed5ddb796f9a6675a5d483204f9999b16174846d
Hamed <hamed.jenabi@gmail.com>
2021-09-03 12:04:58 +0200

    add logo

--- pages/_document.js
+++ pages/_document.js
@@ -16,7 +16,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap"
             rel="stylesheet"
           ></link>
-          <link rel="icon" href="/icon.png" />
+          <link rel="icon" href="/logo.jpeg" />
           <meta name="title" content="Blues in Vienna" />
           <meta
             name="description"
